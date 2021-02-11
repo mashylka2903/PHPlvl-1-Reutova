72372c02841c3a1b32181a6340ae056fcb17a623
Maria Reutova <mashylka2903@gmail.com>
2021-02-11 18:53:29 +0200

    first commit

---
+++ config/main.php
@@ -0,0 +1,8 @@
+<?php
+
+define("ROOT_DIR", realpath(__DIR__. "/../"));
+define("ENGINE_DIR", ROOT_DIR . "/engine/");
+define("CONFIG_DIR", ROOT_DIR . "/config/");
+define("DB_DIR", ROOT_DIR . "/db/");
+define("PUBLIC_DIR", ROOT_DIR . "/public/");
+define("IMG_DIR", PUBLIC_DIR . "/img/");
---
+++ engine/render.php
@@ -0,0 +1,16 @@
+<?php
+
+function displayMenu(array $menu) {
+    echo '<ul class="nav">';
+    foreach ($menu as $item) {
+        echo '<li class="nav-item">
+        <a class="nav-link" href="'.$item['link'].'">';
+        echo $item['title'];
+        echo '</a>
+        </li>';
+    }
+
+    echo '</ul>';
+}
+
+?>
